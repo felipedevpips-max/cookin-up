cb73f3619a0fa236d11ac2f4c8194b3db3054b78
felipedevpips-max <felipedevpips@gmail.com>
2026-01-08 10:46:13 -0300

    aplicando vfor

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+
+  export default {
+    components: { Banner, ConteudoPrincipal}
+    
+  }
+</script>
+
+
+<template>
+      <Banner />
+      <ConteudoPrincipal />
+
+     
+    </template>
+
+
+<style scoped> /* o scoped impede do css vazar*/
+
+</style>
